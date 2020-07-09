51b3ef5f675057324d8ff529656012ed0b2ae8b5
martinKindall <mart256@gmail.com>
2020-07-08 23:11:37 -0400

    Created TaxiRide datatype based on json read

---
+++ src/main/scala/org/myspark/dataTypes/TaxiRide.scala
@@ -0,0 +1,13 @@
+package org.myspark.dataTypes
+
+import play.api.libs.json.{Reads, JsPath}
+import play.api.libs.functional.syntax._
+
+case class TaxiRide(ride_id: String, pointIdx: Int)
+
+object TaxiRide {
+  implicit val apiReads: Reads[TaxiRide] = (
+    (JsPath \ "ride_id").read[String] and
+      (JsPath \ "point_idx").read[Int]
+    )(TaxiRide.apply _)
+}
