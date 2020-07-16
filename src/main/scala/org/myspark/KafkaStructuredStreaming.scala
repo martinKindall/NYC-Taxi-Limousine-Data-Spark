47aa1e5793c289a4db06f8b83d2239f46ed1a6b7
martinKindall <mart256@gmail.com>
2020-07-15 23:14:42 -0400

    Properly showing in console stream as structured taxi data

--- src/main/scala/org/myspark/KafkaStructuredStreaming.scala
+++ src/main/scala/org/myspark/KafkaStructuredStreaming.scala
@@ -9,6 +9,7 @@ import org.apache.spark.sql.functions._
 class KafkaStructuredStreaming(taxiStruct: StructType) extends java.io.Serializable {
   private val sparkCtx: SparkSession  = SparkSession.builder()
     .getOrCreate()
+  import sparkCtx.implicits._
 
   def run(): Unit = {
     val df = sparkCtx
@@ -19,13 +20,25 @@ class KafkaStructuredStreaming(taxiStruct: StructType) extends java.io.Serializa
       .option("startingOffsets", "earliest")
       .load()
 
-    val query = df
-    .writeStream
-    .format("kafka")
-    .option("kafka.bootstrap.servers", "localhost:9092")
-    .option("topic", "taxi-sink")
-    .option("checkpointLocation", "C:\\tmp\\hive\\checkpoints")
-    .start()
+    val query= df.select(
+        col("key").cast("string"),
+        from_json(
+          col("value").cast("string"),
+          taxiStruct).alias("taxi_ride")
+      ).select("taxi_ride.*")
+      /*
+      .withWatermark("timestamp", "60 seconds")
+      .groupBy(
+        window($"timestamp", "60 seconds", "10 seconds")
+      )
+      .sum("meterIncrement")
+      .toJSON
+      .toDF("value")
+      */
+      .writeStream
+      .outputMode("update")
+      .format("console")
+      .start()
 
     query.awaitTermination()
   }
